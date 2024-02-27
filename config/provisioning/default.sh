5492b1c3bbe3bbc1e2633b16ab73b88befe888bf
fysou17 <fysoul17@hotmail.com>
2024-02-27 22:56:38 +0900

    fix model dl link

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -20,8 +20,8 @@ NODES=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://civitai.com/models/43331?modelVersionId=126470" # https://civitai.com/models/43331
-    "https://civitai.com/models/129666?modelVersionId=356366" # ⋅ ⊣ Realities Edge XL ⊢ ⋅ LIGHTNING
+    "https://civitai.com/api/download/models/126470" # majicMIX realistic
+    "https://civitai.com/api/download/models/356366" # ⋅ ⊣ Realities Edge XL ⊢ ⋅ LIGHTNING
     # "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
